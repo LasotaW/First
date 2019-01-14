8fb95084418340e2ada9021b1b81d9ad7fbb51a0
Wojtycha <37120203+Wojtycha@users.noreply.github.com>
2019-01-14 09:42:15 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 #include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Snake!");
+	int x = 0, y = 0;
+	int dir = 0;
+	int textureSize = 16;
+	const float delay = 0.1; //time between snake's moves
+	int size = 2; //start value of snake's size
+
+	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML Snake!");
 	sf::Event event;
 
 	// declare and define texture variables //
@@ -14,30 +21,96 @@ int main()
 	sf::Sprite sprite1(t1);
 	sf::Sprite sprite2(t2);
 
-	int textureSize = 16;
+	sf::Clock clock;
+	sf::Time time;	//important to make comparison with delay
 
-	while (window.isOpen())
+	while (window.isOpen())	//main window's loop
 	{
-		while (window.pollEvent(event))
+		while (window.pollEvent(event))	//event to close the window
 		{
 			window.display();
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
-		window.clear();
-
-		for (int i = 0; i < 800; i += 16)
+		for (int i = 0; i < 400; i += 16)	//draw snake board
 		{
-			for (int j = 0; j < 600; j += 15)
+			for (int j = 0; j < 400; j += 16)
 			{
 				sprite1.setPosition(i, j);
 				window.draw(sprite1);
 			}
 		}
+
+		 //specifies direction of snake's move
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && dir != 1)
+			dir = 0;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && dir != 0)
+			dir = 1;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && dir != 3)
+			dir = 2;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && dir != 2)
+			dir = 3;
+
+		time = clock.getElapsedTime();
+			if (time.asSeconds() > delay)
+			{
+				switch (dir)
+				{
+				case 0:
+					if (x >= 400)
+						x = 0;
+
+					sprite2.setPosition(x, y);
+					window.draw(sprite2);
+					sprite2.setPosition(x - 16, y);
+					window.draw(sprite2);
+					window.display();
+					x += 16;
+					clock.restart();
+					break;
+
+				case 1:
+					if (x <= 0)
+						x = 400;
+
+					sprite2.setPosition(x, y);
+					window.draw(sprite2);
+					sprite2.setPosition(x + 16, y);
+					window.draw(sprite2);
+					window.display();
+					x -= 16;
+					clock.restart();
+					break;
+
+				case 2:
+					if (y >= 400)
+						y = 0;
+
+					sprite2.setPosition(x, y);
+					window.draw(sprite2);
+					sprite2.setPosition(x, y - 16);
+					window.draw(sprite2);
+					window.display();
+					y += 16;
+					clock.restart();
+					break;
+
+				case 3:
+					if (y <= 0)
+						y = 400;
+
+					sprite2.setPosition(x, y);
+					window.draw(sprite2);
+					sprite2.setPosition(x, y + 16);
+					window.draw(sprite2);
+					window.display();
+					y -= 16;
+					clock.restart();
+					break;
+				}
+			}
 			
-
-
 	}
 	return 0;
 }
